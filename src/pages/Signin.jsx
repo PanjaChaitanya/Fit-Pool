674e37e07fa1a5539a40f770abd468c956991c8a
PanjaChaitanya <panjachaitanya23@gmail.com>
2025-03-23 12:35:50 +0530

    sign in created

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,34 @@
-
-
+import { TextField,Button } from "@mui/material"
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div 
+    className="login-container 
+    flex justify-center items-center min-h-screen
+     bg-red-400 relative">
+      <div className="from-box flex flex-col">
+        <h2>Sign in</h2>
+        <form action="#">
+          <div className="input-box flex flex-col">
+            <TextField
+              variant="standard"
+              label="Email"
+              placeholder="Enter your mail id"
+            />
+            <TextField
+              variant="standard"
+              label="Password"
+              placeholder="Enter your password"
+            />
+            <Button color="error"
+            variant="contained"
+            >Login </Button>
+            <div className="signup-link">
+              <p>Dont have an account? <span className="text-red-600"><a href="#">Signup</a></span></p>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
