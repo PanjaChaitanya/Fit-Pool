fa0df374b01ab75ce9a022062e9bad0a98209cd4
PanjaChaitanya <panjachaitanya23@gmail.com>
2025-03-25 18:11:23 +0530

    working on sign in

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -39,22 +39,24 @@ const Signup = ( ) => {
   return (
     <>
       <div className=" signup-body flex items-center justify-center min-h-screen">
-        <div className='signup-box flex flex-wrap border-2 w-2xl border-amber-300 rounded-xl shadow-2xl'>
-          <div className="signup-info w-1/2">
+        <div className='signup-box flex flex-wrap it border-2 w-3xl border-amber-300 rounded-xl shadow-2xl'>
+          <div className="signup-info relative w-1/2">
+            <div className="round-color">
+
+            </div>
           </div>
-          <div className="form-info flex flex-col items-center justify-center p-5">
+          <div className="form-info w-1/2 flex flex-col gap-2 items-center justify-center">
             <h2 className="text-center text-2xl font-semibold text-red-600 mb-4">
               Sign Up
             </h2>
             <form onSubmit={onSignUp}>
-              <div className="form-fields flex flex-col">
-              <TextField 
-                label='User Name' type="text" variant='standard' placeholder="Enter user name"
-                value={userName} onChange={(e)=>setUserName(e.target.value)} 
-                required inputProps={{ minLength: 4 }}
-                className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
-              />
-              
+              <div className="form-fields flex flex-col gap-2">
+                <TextField 
+                  label='User Name' type="text" variant='standard' placeholder="Enter user name"
+                  value={userName} onChange={(e)=>setUserName(e.target.value)} 
+                  required inputProps={{ minLength: 4 }}
+                  className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+                />
                 <TextField
                   label="Email Address"
                   variant='standard'
@@ -75,36 +77,33 @@ const Signup = ( ) => {
                   placeholder="Enter password"
                   className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
                 />
-                  <TextField
-                    label='Confirm Password'
-                    type="password"
-                    variant='standard'
-                    color= {redClass}
-                    value={confirmPassword}
-                    onChange={(e)=>setConfirmPassword(e.target.value)}
-                    required
-                    placeholder="Re-Enter password"
-                    className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
-                  />
+                <TextField
+                  label='Confirm Password'
+                  type="password"
+                  variant='standard'
+                  color= {redClass}
+                  value={confirmPassword}
+                  onChange={(e)=>setConfirmPassword(e.target.value)}
+                  required
+                  placeholder="Re-Enter password"
+                  className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+                />
               </div>
                   {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
-                  <div className="flex items-center justify-center gap-4">
-                    <button type="submit" className="w-full bg-red-600 text-white px-4 py-2 rounded-full hover:bg-red-700">
-                      Sign Up
-                    </button>
-                    <button type="button" className="w-full bg-gray-300 text-gray-700 px-4 py-2 rounded-full hover:bg-gray-400">
-                      Sign Up with Google
-                    </button>
-                  </div>
+              <div className="flex flex-col items-center justify-center gap-2">
+                <button type="submit" className="w-full bg-red-600 text-white px-4 py-2 rounded-full hover:bg-red-700">
+                  Sign Up
+                </button>
+                <button type="button" className="w-full bg-gray-300 text-gray-700 px-4 py-2 rounded-full hover:bg-gray-400">
+                  Sign Up with Google
+                </button>
+              </div>
             </form>
-          
             <div className="mt-4 text-center">
               <button className="text-sm text-blue-600 hover:underline">
                 Already have an account?
               </button>
-
             </div>
-        
           </div>
         </div>
       </div>
